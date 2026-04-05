54a10d51ee65ab153733770cbc84907731a12b33
Stuart Watt <stuart@morungos.com>
2026-04-05 16:18:22 -0300

    switch package

--- userpatches/extensions/rk3588-opencl.sh
+++ userpatches/extensions/rk3588-opencl.sh
@@ -42,7 +42,7 @@ function post_install_kernel_debs__install_minimal_rk3588_opencl() {
 	declare -a pkgs=("libdrm2")
 	pkgs+=("libdrm-common")
 	pkgs+=("clinfo")
-	pkgs+=("opencl-icd")
+	pkgs+=("mesa-opencl-icd")
 	pkgs+=("opencl-c-headers")
 
     local mali_driver_url="https://github.com/ginkage/libmali-rockchip/releases/download/v1.9-1-4b399ed/libmali-valhall-g610-g24p0-dummy_1.9-1_arm64.deb"
